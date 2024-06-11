38aeff73154c64d3a2452e600ce1c5c9e1e2b221
I'mFoodboy <vnn159357@gmail.com>
2024-06-11 12:43:19 +0700

    pull request

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define("HOST_DB", 'localhost');
 define("USER_DB", 'root');
-define("PASS_DB", 'mysql');
+define("PASS_DB", '');
 define("DB", 'db_grademanagement');
 
 define ('ROOT', dirname(__FILE__));
